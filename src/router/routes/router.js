438b3f36b646ef3609d4a94a702755ac8ad71938
Ndhossain <hhhssnahiid@gmail.com>
2022-10-25 01:06:02 +0600

    header and login page ui work done

--- src/router/routes/router.js
+++ src/router/routes/router.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../../layout/Layout";
+import Blog from "../pages/Blog";
 import CourseDetails from "../pages/CourseDetails";
+import CoursePage from "../pages/CoursePage";
 import Courses from "../pages/Courses";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -12,8 +14,10 @@ const router = createBrowserRouter([
         element: <Layout />,
         children: [
             {path: '/', element: <Home />},
+            {path: '/blog', element: <Blog />},
             {path: '/login', element: <Login />},
-            {path: '/regestration', element: <Register />},
+            {path: '/register', element: <Register />},
+            {path: '/courses', element: <CoursePage />},
             {path: '/courses/:id', element: <Courses />},
             {path: '/coursedetail/:id', element: <CourseDetails />},
         ]
